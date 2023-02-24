082b91c01af700ec39c5b5cdb54987bfa7318163
axer665 <vellsevul@mail.ru>
2023-02-24 13:04:06 +0300

    Initial commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[]  args) {
+        String host = "127.0.0.1";
+        int port = 8089;
+        Scanner scanner = new Scanner(System.in);
+
+        try (
+                Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+
+            out.println("Misha");
+            String resp = in.readLine();
+            System.out.println(resp);
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
